dd27353232ac6e6d10b1ff6345d76c184fa8f28b
oladejo abdullahi <67918882+maxwizardth@users.noreply.github.com>
2023-05-06 23:55:18 +0200

    Update polynomial.py

--- polynomial.py
+++ polynomial.py
@@ -1,14 +1,11 @@
 from itertools import permutations,product 
 # Get all permutations
 import numpy as np
-myS=np.full((3,3,3), 0).tolist()
-index=[]
 def permutation(arr):
     index=[]
     perm=permutations(arr)
     [index.append(list(i)) for i in list(perm) if list(i) not in index]
     return index
-
 
 def combinations_with_replacement(iterable, r):
     pool = tuple(iterable)
@@ -17,32 +14,10 @@ def combinations_with_replacement(iterable, r):
         if sorted(indices) == list(indices):
             yield tuple(pool[i] for i in indices)
 
-def combine(n,r):
-    iterable=[i for i in range(n+1)]
-    com=combinations_with_replacement(iterable,r)
-    print(com)
-    index1=[]
-    for i in list(com):
-        if n==sum(list(i)):
-            [index1.append(el) for el in permutation(list(i))]
-    return {"powers":index1,"dimension":len(index1)}
-
-
-def polynomial(degree,variables):
-    powersArray=combine(degree,variables)['powers']
-    m=[]
-    for powerItems in powersArray:
-        term=[(variablesWithPower(i, powerItems[i])) for i in range(variables)]
-        m.append(''.join(map(str,term)))
-    return '+'.join(map(str,m))
-        
-
 # function to convert to subscript
 def sub(x):
-  base=['\u2080','\u2081','\u2082','\u2083',
-         '\u2084','\u2085','\u2086','\u2087',
-         '\u2088','\u2089']
-  return(base[x])
+    base=['\u2080','\u2081','\u2082','\u2083','\u2084','\u2085','\u2086','\u2087','\u2088','\u2089']
+    return(base[x])
          
 def sup(x):
   power= ['\u2070','\u2071','\u00b2','\u00b3',
@@ -50,6 +25,8 @@ def sup(x):
          '\u2078','\u2079']
   return(power[x])
          
+
+
 def variablesWithPower(xi,power):
     if power==0:
         return ''
@@ -58,5 +35,40 @@ def variablesWithPower(xi,power):
     else:
         return 'x'+sub(xi)+sup(power)
  
+class Polynomials:
+  def __init__(self, deg, var):
+    self.degree = deg
+    self.variable = var
+    self.Basis=self.Basis()
+    self.Dimension=len(self.Basis)
+    self.function='+'.join(map(str,self.Basis))
+  
+  def __repr__(self):
+      return self.function
+    
+  def Basis(self):
+      degree=self.degree
+      variables=self.variable
+      powersArray=self.combine()
+      m=[]
+      for powerItems in powersArray:
+          term=[(variablesWithPower(i, powerItems[i])) for i in range(variables)]
+          m.append(''.join(map(str,term)))
+      return m
+  
+  def combine(self):
+      n=self.degree
+      r=self.variable
+      
+      iterable=[i for i in range(n+1)]
+      com=combinations_with_replacement(iterable,r)
+      index1=[]
+      for i in list(com):
+          if n==sum(list(i)):
+              [index1.append(el) for el in permutation(list(i))]
+      return index1
 
-print(polynomial(2, 3))
+
+ 
+x = Polynomials(2,3)
+print(x.Dimension)
